addfecfad4eb37f77cccc7bf5dcdc1d5aaea3b63
ThiagoRovetta <rovetta.thiago@gmail.com>
2022-05-06 08:55:52 -0300

    Created admin seed.

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,13 +2,14 @@ import express, { Request, Response, NextFunction } from 'express';
 import swaggerUI from 'swagger-ui-express';
 import 'express-async-errors';
 
-import '@shared/infra/typeorm';
 import '@shared/container';
 import { AppError } from '@shared/errors/AppError';
 import { router } from '@shared/infra/http/routes';
+import createConnection from '@shared/infra/typeorm';
 
 import swaggerFile from '../../../swagger.json';
 
+createConnection();
 const app = express();
 
 app.use(express.json());
