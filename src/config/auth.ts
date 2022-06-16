e364b52fcce3fed493d1caa432c6832325b4e838
ThiagoRovetta <rovetta.thiago@gmail.com>
2022-06-15 23:14:31 -0300

    Creating refresh token.

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: '7d9f9bb5e510693cdf614678b0a17f22',
+  expires_in_token: '15m',
+  secret_refresh_token: '9be09ab73fb6e8da1cae9ed6b779f6ab',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+};
