3a30909494dde73c844b2cc2beece8db25b6ad16
ThiagoRovetta <rovetta.thiago@gmail.com>
2022-07-18 23:14:41 -0300

    Added sentry configuration.

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,10 +2,12 @@ import 'dotenv/config';
 import cors from 'cors';
 import express, { Request, Response, NextFunction } from 'express';
 import swaggerUI from 'swagger-ui-express';
-import 'express-async-errors';
 
-import '@shared/container';
+import 'express-async-errors';
 import upload from '@config/upload';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
+import '@shared/container';
 import { AppError } from '@shared/errors/AppError';
 import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
 import { router } from '@shared/infra/http/routes';
@@ -14,9 +16,22 @@ import createConnection from '@shared/infra/typeorm';
 import swaggerFile from '../../../swagger.json';
 
 createConnection();
+
 const app = express();
 
 app.use(rateLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
 
@@ -27,6 +42,8 @@ app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if (err instanceof AppError) {
